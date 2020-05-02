94b403c96532a6590b5f3168b48d020305479f3e
DurandGasselinSylvain17 <64553040+DurandGasselinSylvain17@users.noreply.github.com>
2020-05-02 12:49:18 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArtisansComponent } from './artisans/artisans.component';
 import { ArtisanComponent } from './artisans/artisan/artisan.component';
 import { ArtisanService } from './shared/artisan.service';
+import { ArtisanItemComponent } from './artisans/artisan-item/artisan-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArtisansComponent,
-    ArtisanComponent
+    ArtisanComponent,
+    ArtisanItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatSliderModule,
+    HttpClientModule
   ],
+  entryComponents: [ArtisanItemComponent],
   providers: [ArtisanService],
   bootstrap: [AppComponent]
 })
--- src/app/artisans/artisan/artisan.component.ts
+++ src/app/artisans/artisan/artisan.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ArtisanService } from '../../shared/artisan.service';
 import { NgForm } from '@angular/forms';
+import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ArtisanItemComponent } from '../artisan-item/artisan-item.component';
+import { Artisan } from '../../shared/artisan.model';
+import { QualificationArtisan } from '../../shared/qualification.model';
+import { environment } from '../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-artisan',
@@ -9,22 +18,61 @@ import { NgForm } from '@angular/forms';
   ]
 })
 export class ArtisanComponent implements OnInit {
+  artisansList: Artisan[];
+  rereshEventSubscription:Subscription;
 
-  constructor(public service: ArtisanService) { }
+  constructor(public service: ArtisanService,
+  public dialog:MatDialog,
+  public http: HttpClient,
+  public router: Router
+) { 
+  this.rereshEventSubscription = this.service.getRefreshEvent().subscribe(()=>{this.incrementCounter();})
+}
 
+count:number=0;
   ngOnInit() {
     this.resetForm();
+    this.rereshEventSubscription = this.service.getRefreshEvent().subscribe(()=>{this.incrementCounter();})
+    this.service.getArtisansList().then(res => this.artisansList = res as Artisan[]);
   }
 
   resetForm(form?:NgForm) {
     if(form = null)
       form.resetForm();
-    this.service.formData = {
-      ArtisanID: null,
-      Nom: 'HellW',
-      QualificationArtisanID:0
-    
   }
+  
+  AddOrEditArtisan(data){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    //dialogConfig.width = "50%";
+    dialogConfig.data = {data};
+    this.dialog.open(ArtisanItemComponent, dialogConfig);
+  }
+
+  DeleteArtisan(data){
+    this.service.deleteArtisan(data);
+    this.ngOnInit();
+  }
+
+  ToAdminMenu(url){
+    // url = 'A définir dans le html'
+
+    // this.router.navigate([url]).then( (e) => {
+      this.router.navigateByUrl(url).then( (e) => {
+      if (e) {
+        console.log("Navigation is successful!");
+      } else {
+        console.log("Navigation has failed!");
+      }
+    });
+  }
+
+  incrementCounter(){
+    
+    this.ngOnInit()
+    this.count ++ ;
+    console.log('compteur' + this.count)
   }
 
 }
